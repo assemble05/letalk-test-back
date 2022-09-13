011722c80d39e926b3bd575e90327d177e8a7b40
assemble05 <joneto18.jh@gmail.com>
2022-09-12 21:47:11 -0300

    basic set up

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+import { appRoutes } from './routes'
+import { errorMiddleware } from './middlewares/error.middleware'
+import "express-async-errors"
+const app = express()
+
+app.use(express.json())
+
+appRoutes(app)
+
+app.use(errorMiddleware)
+
+app.listen(3000)
